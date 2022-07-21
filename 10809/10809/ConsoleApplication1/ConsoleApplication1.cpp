a6df1aee4e6512d2136671f2348f15aeefda6592
unwind <ysh038@naver.com>
2022-07-21 16:16:30 +0900

    1157, 10809

---
+++ 10809/10809/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    int start = (int)'a';
+    int end = (int)'z';
+    for (int i = start; i <= end; i++) {
+        int result = S.find((char)i);
+        cout << result << " ";
+    }
+    return 0;
+}
