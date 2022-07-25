185f5da2933a9925fe816a6b871cc744ebd32fbe
unwind <ysh038@naver.com>
2022-07-25 17:34:21 +0900

    1152

--- 10809/10809/ConsoleApplication1/ConsoleApplication1.cpp
+++ 10809/10809/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,5 @@
-﻿#include<iostream>
+﻿/* 10809
+#include<iostream>
 #include <string>
 
 using namespace std;
@@ -13,4 +14,35 @@ int main() {
         cout << result << " ";
     }
     return 0;
-}
+}*/
+
+/* 1152 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int result = 0;
+    bool word_start = false;
+    string S;
+    getline(cin, S);
+    for (int i = 0; i < S.length(); i++) {
+        if (S[i] != ' ' && word_start == false) {
+            word_start = true;
+        }
+        else if (S[i] == ' ' && word_start == true && S[i + 1] != NULL) {
+            word_start = false;
+            result++;
+        }
+    }
+    bool isBlank = false;
+    for (int i = 0; i < S.length(); i++) {
+        if (S[i] != ' ')
+            isBlank = true;
+    }
+    if(isBlank){
+        result++;
+    }
+    cout << result;
+}*/
