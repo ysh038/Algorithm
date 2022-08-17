5f15aeaf639fb25ea79c4f19488fb3bdcc74c626
unwind <ysh038@naver.com>
2022-08-17 16:59:22 +0900

    2750 수 정렬하기
    2750 수 정렬하기
    삽입정렬, 버블정렬 두 가지
    방법을 사용

--- Solution/Solution/ConsoleApplication1/ConsoleApplication1.cpp
+++ Solution/Solution/ConsoleApplication1/ConsoleApplication1.cpp
@@ -316,3 +316,67 @@ int main() {
     }
     cout << result;
 }*/
+/*2750 Bubble Sort
+#include <iostream>
+using namespace std;
+
+void swap(int* first, int* second) {
+    int temp = *second;
+    *second = *first;
+    *first = temp;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    int* arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 1; j < N - i; j++) {
+            if (arr[j - 1] > arr[j]) {
+                swap(&arr[j - 1], &arr[j]);
+            }
+        }
+    }
+    
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}*/
+/*2750 Insertion Sort
+#include <iostream>
+using namespace std;
+
+void swap(int* first, int* second) {
+    int temp = *second;
+    *second = *first;
+    *first = temp;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    int* arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < N; i++) {
+        for (int j = i-1; j >= 0; j--) {
+            if (arr[j+1] < arr[j]) {
+                swap(&arr[j + 1], &arr[j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}*/
