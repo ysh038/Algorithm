bfb857f454f224811b64d3579d94407f947d6cf6
[username] <ysh038@naver.com>
2023-09-15 17:56:02 +0900

    10810

--- python/1차원 배열/10810/10810.py
+++ python/1차원 배열/10810/10810.py
@@ -1,7 +1,11 @@
 N,M=map(int,input().split())
 
-array=[]
+array= [0] * (N)
 
 for i in range(M):
     i,j,k = map(int,input().split())
-    
+    for index in range(i-1,j):
+        array[index] = k
+
+for i in array:
+    print(i,end=" ")
