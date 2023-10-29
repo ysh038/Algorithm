36faf19c309ef0679cb4b5d8089128e8c14d6535
unwind <ysh038@naver.com>
2023-10-30 00:57:42 +0900

    10816

--- 백준/python/집합과 맵/10816/10816.py
+++ 백준/python/집합과 맵/10816/10816.py
@@ -1,21 +1,24 @@
 import sys
 
 N = int(sys.stdin.readline())
-
-sangguen_card = {}
-
-for i in range(N):
-    sangguen_card[i] = int(sys.stdin.readline())
+sangguen_card_array = []
+sangguen_card_array = list(map(int,sys.stdin.readline().split()))
 
 M = int(sys.stdin.readline())
+compare_card_array = []
+compare_card_array = list(map(int,sys.stdin.readline().split()))
 
 compare_card_set = {}
 
-for i in range(M):
-    compare_card_set[int(sys.stdin.readline())] = i
+for i in compare_card_array:
+    compare_card_set[i] = 0
 
-# for i in sangguen_card:
-#     if i in compare_card_set:
-#         print("shit")
+for i in sangguen_card_array:
+    if i in compare_card_set:
+        compare_card_set[i] += 1
 
-print(compare_card_set)
+for i in compare_card_array:
+    if i in compare_card_set:
+        print(compare_card_set[i], end=" ")
+    else:
+        print(0, end=" ")
