2d477b53619039a25db4d2cb5bfac2d85fd3db9f
unwind <ysh038@naver.com>
2023-12-28 19:25:21 +0900

    20920

--- 백준/python/심화 2/20920/20920.py
+++ 백준/python/심화 2/20920/20920.py
@@ -10,4 +10,19 @@ for i in range(N):
     else:
         word_array.append(word)
 
-set(word_array)
+word_array.sort(key=lambda x : (-len(x),x))
+
+word_dict = {}
+for word in word_array:
+    if word in word_dict:
+        word_dict[word] += 1
+    else:
+        word_dict[word] = 1
+sorted_with_frequency = sorted(word_dict.items(),key=lambda x: x[1], reverse=True)
+
+sorted_with_frequency_list = []
+for word in sorted_with_frequency:
+    sorted_with_frequency_list.append(word[0]) # 빈도 수를 기준으로 정렬한 key, value 쌍의 list 의 key값으로 다시 list만들기
+
+for word in sorted_with_frequency_list:
+    print(word)
