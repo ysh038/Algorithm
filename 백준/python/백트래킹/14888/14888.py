4aaefe8e5fbe989ce0dff608e94072991e03ce04
unwind <ysh038@naver.com>
2024-01-19 17:58:31 +0900

    14888

---
+++ 백준/python/백트래킹/14888/14888.py
@@ -0,0 +1,38 @@
+import sys
+
+N = int(sys.stdin.readline())
+array = list(map(int, sys.stdin.readline().split()))
+add, sub, mul, div = map(int, sys.stdin.readline().split())
+
+max_result = - int(1e9)
+min_result = int(1e9)
+
+def dfs(add, sub, mul, div, sum,idx):
+    global result
+    global max_result
+    global min_result
+
+    if idx == N:
+        max_result = max(max_result, sum)
+        min_result = min(min_result, sum)
+        return 
+    
+    if add:
+        dfs(add-1, sub, mul, div, sum+array[idx],idx+1)
+
+    if sub:
+        dfs(add, sub-1, mul, div, sum-array[idx],idx+1)
+
+    if mul > 0:
+        dfs(add, sub, mul-1, div,sum*array[idx],idx+1)
+
+    if div > 0:
+        dfs(add, sub, mul, div-1, int(sum/array[idx]),idx+1)
+
+
+    array[idx]
+
+dfs(add, sub, mul, div, array[0],1)
+
+print(max_result)
+print(min_result)
