019d39ee7ea60365d6e5016c6e7f51cb4062c3eb
unwind <ysh038@naver.com>
2024-01-31 23:09:49 +0900

    2579

--- 백준/python/동적 계획법 1/2579/2579.py
+++ 백준/python/동적 계획법 1/2579/2579.py
@@ -1,15 +1,15 @@
 import sys
 
 N = int(sys.stdin.readline())
-array = []
-dp = [0] * (N)
+array = [0] * 301
+dp = [0] * 301
 
 for i in range(N):
-    array.append(int(sys.stdin.readline()))
+    array[i] = int(sys.stdin.readline())
 
 dp[0] = array[0]
 dp[1] = array[1] + dp[0]
-dp[2] = max(array[0]+array[1],array[1]+array[2])
+dp[2] = max(array[0]+array[2],array[1]+array[2])
 
 for i in range(3,N):
     dp[i] = max(dp[i-3] + array[i-1] + array[i],dp[i-2] + array[i])
