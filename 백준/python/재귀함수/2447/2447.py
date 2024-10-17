a83be2039ca5cc7194095588d5685435a2f0269c
unwind <ysh038@naver.com>
2024-10-17 22:05:07 +0900

    2447 GOLD 5

--- 백준/python/재귀함수/2447/2447.py
+++ 백준/python/재귀함수/2447/2447.py
@@ -1,29 +1,22 @@
 import sys
 
-Number = int(sys.stdin.readline())
-
+N = int(sys.stdin.readline())
 rect = []
 
-def make(N):
-    if N > 0:
-        for width in range(Number):
-            array = []
-            for height in range(Number):
-                if (height >= int(N/3) and height < int((N/3)*2)) and (width >= int(N/3) and width < int((N/3)*2)):
-                    # print(" ",end="")
-                    array.append(" ")
-                else:
-                    # print("*",end="")
-                    array.append("*")
-            array.append("\n")
-            rect.append(array)
-            # print("")
-        if int(N/3) != 1:
-            return make(int(N/3))
-        
-make(Number)
+def recursion(new_N):
+  if new_N == 1:
+    return "*"
+  else:
+    stars = recursion(new_N // 3)
+    array = []
 
-for i in range(Number):
-    for j in range(Number):
-        print(rect[i][j],end="")
-    print("")
+    for star in stars:
+      array.append(star * 3)
+    for star in stars:
+      array.append(star + ' ' * (new_N // 3) + star)
+    for star in stars:
+      array.append(star * 3)
+    
+    return array
+
+print('\n'.join(recursion(N)))
