5a0f5b88334a898e48d0d527973988fa20f1ef3d
unwind <ysh038@naver.com>
2024-10-18 22:15:56 +0900

    11729 하노이 탑

---
+++ 백준/python/재귀함수/11729/11729.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+def recursion(n,start,mid,end):
+  if n == 1:
+    print(start,end)
+    return
+  recursion(n-1,start,end,mid)
+  print(start,end)
+  recursion(n-1,mid,start,end)
+print(2 ** N - 1)
+recursion(N,1,2,3)
