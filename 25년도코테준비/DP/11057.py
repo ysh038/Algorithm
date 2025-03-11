fe619754ab90dd8109de48ef2bf6e4bbdada77f3
unwind <ysh038@naver.com>
2025-03-11 22:12:07 +0900

    11057

---
+++ 25년도코테준비/DP/11057.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [[0]*10 for _ in range(n)]
+
+for i in range(10):
+    dp[0][i] = 1
+
+for i in range(1,n):
+    dp[i][0] = 1
+    for j in range(1,10):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+result = 0
+for i in range(10):
+    result += dp[n-1][i]
+print(result%10007)
