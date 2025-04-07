3c6039dcb7429e1cce01aed373633299db9c19d3
unwind <ysh038@naver.com>
2025-04-07 22:02:36 +0900

    2751

--- 25년도코테준비/이런저런문제/2751.py
+++ 25년도코테준비/이런저런문제/2751.py
@@ -6,12 +6,39 @@ array =[]
 for _ in range(N):
     array.append(int(sys.stdin.readline()))
 
-for j in range(N):
-    for i in range(N-1): 
-        if array[i] > array[i+1]:
-            temp = array[i+1]
-            array[i+1] = array[i]
-            array[i] = temp
+def merge(left_array, right_array):
+    new_array = []
+    left_index,right_index = 0,0
+    
+    while left_index < len(left_array) and right_index < len(right_array):
+        if left_array[left_index] > right_array[right_index]:
+            new_array.append(right_array[right_index])
+            right_index+=1
+        else:
+            new_array.append(left_array[left_index])
+            left_index+=1
 
-for i in range(N):
-    print(array[i])
+    while left_index < len(left_array):
+        new_array.append(left_array[left_index])
+        left_index+=1
+    while right_index < len(right_array):
+        new_array.append(right_array[right_index])
+        right_index+=1
+
+    return new_array
+
+def sort(array):
+    if len(array) <= 1:
+        return array
+    
+    middle = len(array) // 2
+    
+    left_array = sort(array[:middle])
+    right_array = sort(array[middle:])
+    
+    return merge(left_array, right_array)
+    
+sorted_array = sort(array)
+
+for i in range(len(array)):
+    print(sorted_array[i])
