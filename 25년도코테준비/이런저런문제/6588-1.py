154102542b7cbca9f54736837e35f2ceae2c0aef
unwind <ysh038@naver.com>
2025-04-24 22:48:45 +0900

    6588 복습

--- 25년도코테준비/이런저런문제/6588-1.py
+++ 25년도코테준비/이런저런문제/6588-1.py
@@ -1,15 +1,31 @@
 import sys
 import math
 
-prime = [True] * 1000001
-prime[0] = False
-prime[1] = False
+prime = [True for _ in range(1000001)]
 
-for i in range(2,int(math.sqrt(1000000))+1):
+prime[0], prime[1] = False, False
+
+for i in range(2,int(math.sqrt(10000001))+1):
+    j=2
     if prime[i] == True:
-        j = 2
-        while(i*j) < 1000001:
+        while(i*j < 1000001):
             prime[i*j] = False
             j+=1
-        
-print(prime[3],prime[10],prime[17])
+
+n = int(sys.stdin.readline())
+
+while(n != 0):
+    if n % 2 != 0:
+        print("Goldbach's conjecture is wrong.")
+
+    found = False
+    for k in range(3, n//2 + 1, 2):  # 홀수 소수만 확인 (2 제외)
+        if prime[k] and prime[n-k]:
+            print(f"{n} = {k} + {n-k}")
+            found = True
+            break
+    
+    if not found:
+        print("Goldbach's conjecture is wrong.")
+            
+    n = int(sys.stdin.readline())
